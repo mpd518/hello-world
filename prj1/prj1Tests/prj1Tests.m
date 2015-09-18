a6506f0b2764c65e8e7a1aabe61fcb03dd7c4739
xiexiaogithub <5313984@qq.com>
2015-09-18 10:14:05 +0800

    test commit

---
+++ prj1/prj1Tests/prj1Tests.m
@@ -0,0 +1,40 @@
+//
+//  prj1Tests.m
+//  prj1Tests
+//
+//  Created by Xiao Xie on 9/18/15.
+//  Copyright (c) 2015 Xiao Xie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface prj1Tests : XCTestCase
+
+@end
+
+@implementation prj1Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
